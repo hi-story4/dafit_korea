fbe6b49806558f6ef426ccdac646395a01aa4739
“hi-story4” <wnsgur1503@naver.com>
2023-12-28 22:04:12 +0900

    이모지 추가

--- app/components/Popup.js
+++ app/components/Popup.js
@@ -20,7 +20,7 @@ export function Popup({children}) {
     return (
         <>
             <div className={popup ?'popupOn' : 'popupOff'}>
-                😥현재는 앱이 출시되지 않았습니다.😥<br/>😊앱 출시 후 많은 관심 부탁드립니다.😊
+                😥 현재는 앱이 출시되지 않았습니다. 😥<br/>😊 앱 출시 후 많은 관심 부탁드립니다. 😊
                 <span className='closeBtn' onClick={()=>{setPopup(false)}}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512" fill='white'>
                         <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
