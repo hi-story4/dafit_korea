cf5e97b605feb9855b7333cafba5513354fc0986
“hi-story4” <wnsgur1503@naver.com>
2023-12-28 22:01:36 +0900

    context API 활용해  모달창state를 전역에서 처리가능하게 만들어 각 버튼들은 모달창을 따로 생성하지 않고 공유함.

--- app/components/Popup.js
+++ app/components/Popup.js
@@ -1,30 +1,35 @@
 'use client'
-import React, { useEffect, useState} from 'react'
-export default function popup({children}) {
+import React, { createContext, useEffect, useState} from 'react'
+
+export const GlobalContext = createContext({});
+
+
+export function Popup({children}) {
     const [popup, setPopup] =  useState(false);
+    const contextValue = {
+        popup,
+        setPopup,
+    };
     useEffect(()=>{
         if(popup){
             setTimeout(() => {
                 setPopup(false);
-            }, 3000);
+            }, 4000);
         }
     },[popup])
     return (
-        < >
-            {
-                popup
-                ?
-                <div className='popup'>
-                    현재는 앱이 출시되지 않았습니다. 앱 출시 후 많은 관심 부탁드립니다.
-                    <span className='closeBtn' onClick={()=>{setPopup(false)}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512" fill='white'>
-                            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
-                        </svg>
-                    </span>
-                </div>
-                : null
-            }
-            {children}
+        <>
+            <div className={popup ?'popupOn' : 'popupOff'}>
+                😥현재는 앱이 출시되지 않았습니다.😥<br/>😊앱 출시 후 많은 관심 부탁드립니다.😊
+                <span className='closeBtn' onClick={()=>{setPopup(false)}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512" fill='white'>
+                        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                    </svg>
+                </span>
+            </div>
+            <GlobalContext.Provider value={contextValue}>
+                {children}
+            </GlobalContext.Provider>
         </>
         
     )
--- app/page.js
+++ app/page.js
@@ -7,94 +7,97 @@ import Nav from './components/Nav'
 import DownloadButton from './components/DownloadButton'
 import AppstoreBtn from './components/AppstoreBtn'
 import PlayStoreBtn from './components/PlayStoreBtn'
+import { Popup } from './components/Popup'
 
 export default function Home() {
   return (
     <div>
-      <header>
-        <div className='hederContainer'>
-          <h4>다핏</h4>
-          <DownloadButton/>
+      <Popup>
+        <header>
+          <div className='hederContainer'>
+            <h4>다핏</h4>
+            <DownloadButton/>
+          </div>
+        </header>
+        <section className='heroSection'>
+          <div className='heroSectionWrap'>
+            <p>다이어트 성공할 수밖에 없는 이유,</p>
+            <h2>24시간 온라인 PT 서비스</h2>
+            <h1>다핏</h1>
+            <div className='buttons'>
+              <PlayStoreBtn/>
+              <AppstoreBtn/>
+            </div>
+          </div>
+        </section>
+        <div className='sections'>
+          <Nav/>
+          <section className='firstSection'>
+            <div className='sectionWrap'>
+              <div className='imgBox notCurrentImg'>
+                <Image src={img1} alt='핸드폰과 동전을 받는 손 이미지'  />
+              </div>
+              <div className='sectionText' >
+                <h2>오프라인 PT에 비해 <br/><span className='line'>저렴한 가격<div className='notcurrentAccent'></div></span></h2>
+                <p>다이어트 성공할 수밖에 없는 이유,
+                <br/>다핏은 <strong>매일 식단 관리부터 운동 코칭까지
+                <br/>해주는 트레이너를 매칭해줘요.</strong>
+                <br/>오프라인 PT보다 무려
+                <br/><strong>평균 40% 이상 저렴해요!</strong></p>
+              </div>
+            </div>
+          </section>
+          <section className='secondSection'>
+            <div className='sectionWrap'>
+              <div className='sectionText' >
+                <h2>나와 맞는 트레이너를<br/>찾을 때까지, <span className='line'>무제한 변경<div className='notcurrentAccent'></div></span></h2>
+                <p>기존 PT에서는
+                <br/>트레이너 선생님을 변경하기 어려웠어요.
+                <br/>하지만, 다핏은 <strong>트레이너 선생님을
+                <br/>원할 때면 언제든 변경</strong>할 수 있어요.
+                </p>
+              </div>
+              <div className='imgBox notCurrentImg'>
+                <Image src={img2} alt='핸드폰과 동전을 받는 손 이미지'  />
+              </div>
+            </div>
+          </section>
+          <section className='thirdSection'>
+          <div className='sectionWrap'>
+              <div className='imgBox notCurrentImg'>
+                <Image src={img3} alt='핸드폰과 동전을 받는 손 이미지'  />
+              </div>
+              
+              <div className='sectionText' >
+                <h2><span className='line'>헬스장에 다니지 않아도,<div className='notcurrentAccent'></div></span><br/>맞춤형 다이어트</h2>
+                <p>꼭 헬스장에 가서
+                <br/>PT를 받으라는 법은 없잖아요!
+                <br/>다핏은 헬스장뿐만 아니라 
+                <br/>홈트레이닝, 야외 스포츠, 실내 스포츠 등
+                <br/><strong>모든 운동을 포괄한
+                <br/>맞춤형 다이어트 코칭</strong>을 제공해요.</p>
+              </div>
+            </div>
+          </section>
+          <section className='fourthSection'>
+            <div className='sectionWrap'>
+              <div className='sectionText' >
+                <h2><span className='line'>운동할 시간이 없어도,<div className='notcurrentAccent'></div></span><br/>맞춤형 다이어트</h2>
+                <p>다핏과 함께라면
+                <br/>다이어트의 시간과 장소에 대한 걱정은
+                <br/>넣어두세요.
+                <br/>이용자님의 <strong>루틴과 생활 패턴에 맞춘 
+                <br/>최상의 다이어트 식단과 운동을
+                <br/>알려드리고 코칭</strong>해드릴게요.</p>
+              </div>
+              <div className='imgBox notCurrentImg'>
+                <Image src={img4} alt='핸드폰과 동전을 받는 손 이미지'  />
+              </div>
+              
+            </div>
+          </section>
         </div>
-      </header>
-      <section className='heroSection'>
-        <div className='heroSectionWrap'>
-          <p>다이어트 성공할 수밖에 없는 이유,</p>
-          <h2>24시간 온라인 PT 서비스</h2>
-          <h1>다핏</h1>
-          <div className='buttons'>
-            <PlayStoreBtn/>
-            <AppstoreBtn/>
-          </div>
-        </div>
-      </section>
-      <div className='sections'>
-        <Nav/>
-        <section className='firstSection'>
-          <div className='sectionWrap'>
-            <div className='imgBox notCurrentImg'>
-              <Image src={img1} alt='핸드폰과 동전을 받는 손 이미지'  />
-            </div>
-            <div className='sectionText' >
-              <h2>오프라인 PT에 비해 <br/><span className='line'>저렴한 가격<div className='notcurrentAccent'></div></span></h2>
-              <p>다이어트 성공할 수밖에 없는 이유,
-              <br/>다핏은 <strong>매일 식단 관리부터 운동 코칭까지
-              <br/>해주는 트레이너를 매칭해줘요.</strong>
-              <br/>오프라인 PT보다 무려
-              <br/><strong>평균 40% 이상 저렴해요!</strong></p>
-            </div>
-          </div>
-        </section>
-        <section className='secondSection'>
-          <div className='sectionWrap'>
-            <div className='sectionText' >
-              <h2>나와 맞는 트레이너를<br/>찾을 때까지, <span className='line'>무제한 변경<div className='notcurrentAccent'></div></span></h2>
-              <p>기존 PT에서는
-              <br/>트레이너 선생님을 변경하기 어려웠어요.
-              <br/>하지만, 다핏은 <strong>트레이너 선생님을
-              <br/>원할 때면 언제든 변경</strong>할 수 있어요.
-              </p>
-            </div>
-            <div className='imgBox notCurrentImg'>
-              <Image src={img2} alt='핸드폰과 동전을 받는 손 이미지'  />
-            </div>
-          </div>
-        </section>
-        <section className='thirdSection'>
-        <div className='sectionWrap'>
-            <div className='imgBox notCurrentImg'>
-              <Image src={img3} alt='핸드폰과 동전을 받는 손 이미지'  />
-            </div>
-            
-            <div className='sectionText' >
-              <h2><span className='line'>헬스장에 다니지 않아도,<div className='notcurrentAccent'></div></span><br/>맞춤형 다이어트</h2>
-              <p>꼭 헬스장에 가서
-              <br/>PT를 받으라는 법은 없잖아요!
-              <br/>다핏은 헬스장뿐만 아니라 
-              <br/>홈트레이닝, 야외 스포츠, 실내 스포츠 등
-              <br/><strong>모든 운동을 포괄한
-              <br/>맞춤형 다이어트 코칭</strong>을 제공해요.</p>
-            </div>
-          </div>
-        </section>
-        <section className='fourthSection'>
-          <div className='sectionWrap'>
-            <div className='sectionText' >
-              <h2><span className='line'>운동할 시간이 없어도,<div className='notcurrentAccent'></div></span><br/>맞춤형 다이어트</h2>
-              <p>다핏과 함께라면
-              <br/>다이어트의 시간과 장소에 대한 걱정은
-              <br/>넣어두세요.
-              <br/>이용자님의 <strong>루틴과 생활 패턴에 맞춘 
-              <br/>최상의 다이어트 식단과 운동을
-              <br/>알려드리고 코칭</strong>해드릴게요.</p>
-            </div>
-            <div className='imgBox notCurrentImg'>
-              <Image src={img4} alt='핸드폰과 동전을 받는 손 이미지'  />
-            </div>
-            
-          </div>
-        </section>
-      </div>
+      </Popup>
       <footer>
         <div className='footerWrap'>
           <div className='footerContents'>
